9c037e38583a3c4c959322e50349289b989cda0a
Denis Cohen <denis.cohen.132@gmail.com>
2021-03-19 11:12:25 +0100

    Update

--- man/examples/statmodeling.R
+++ man/examples/statmodeling.R
@@ -1,14 +1,14 @@
 \dontrun{
   # Introduction
-  learnr::run_tutorial("0-int", package = "statmodeling")
+  learnr::run_tutorial("00-int", package = "statmodeling")
 
   # Session 1
-  learnr::run_tutorial("1-lec", package = "statmodeling")
-  learnr::run_tutorial("1-lab", package = "statmodeling")
+  learnr::run_tutorial("01-lec", package = "statmodeling")
+  learnr::run_tutorial("01-lab", package = "statmodeling")
 
   # Session 2
-  learnr::run_tutorial("2-lec", package = "statmodeling")
-  learnr::run_tutorial("2-lab", package = "statmodeling")
+  learnr::run_tutorial("02-lec", package = "statmodeling")
+  learnr::run_tutorial("02-lab", package = "statmodeling")
 
-  ...
+  # etc
 }
