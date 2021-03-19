0b321637ce0b7df1e0497fc6e4c1edfbdc8f0681
Denis Cohen <denis.cohen.132@gmail.com>
2021-03-19 10:51:50 +0100

    Update materials

---
+++ man/examples/statmodeling.R
@@ -0,0 +1,14 @@
+\dontrun{
+  # Introduction
+  learnr::run_tutorial("0-int", package = "statmodeling")
+
+  # Session 1
+  learnr::run_tutorial("1-lec", package = "statmodeling")
+  learnr::run_tutorial("1-lab", package = "statmodeling")
+
+  # Session 2
+  learnr::run_tutorial("2-lec", package = "statmodeling")
+  learnr::run_tutorial("2-lab", package = "statmodeling")
+
+  ...
+}
