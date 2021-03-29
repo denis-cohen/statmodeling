28911e9cd16c01db24516b351b196e89c9de27a7
Denis Cohen <denis.cohen.132@gmail.com>
2021-03-29 08:46:10 +0200

    Upload Day 1

---
+++ R/statmodeling.R
@@ -0,0 +1,17 @@
+#' statmodeling: Statistical Modeling with R and Stan
+#'
+#' @description The \code{statmodeling} package provides a collection of
+#' interactive \code{learnr} materials for the workshop
+#' "Statistical Modeling with R and Stan". The workshop constists of six
+#' sessions, each divided into a lecture and a lab. You can call the
+#' materials for each lecture and lab using the code provided in the examples
+#' below.
+#'
+#' @seealso \code{\link{learnr}}
+#'
+#' @example man/examples/statmodeling.R
+#'
+#' @docType package
+#' @name statmodeling
+NULL
+#> NULL
---
+++ man/examples/statmodeling.R
@@ -0,0 +1,14 @@
+\dontrun{
+  # Introduction
+  learnr::run_tutorial("00-int", package = "statmodeling")
+
+  # Session 1
+  learnr::run_tutorial("01-lec", package = "statmodeling")
+  learnr::run_tutorial("01-lab", package = "statmodeling")
+
+  # Session 2
+  learnr::run_tutorial("02-lec", package = "statmodeling")
+  learnr::run_tutorial("02-lab", package = "statmodeling")
+
+  # etc
+}
